c2abfcf3dd53eab8fa7ef38e506130151c5c3555
Raj.Krishna1 <raj.krishna1@ust.com>
2021-05-10 08:40:27 +0530

    zoamto

---
+++ src/main/java/com/ust/ZomatoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ust;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZomatoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZomatoServiceApplication.class, args);
+	}
+
+}
